b4d002648a2c23df873d1d70cdf44d73f0bfd659
Marek Grabarczyk <grabarczykmarek@gmail.com>
2024-02-27 20:04:19 +0100

    test Commit

--- src/test/java/pl/javaready/readerArtilceRetriever/ReaderArticleRetrieverFacadeTest.java
+++ src/test/java/pl/javaready/readerArtilceRetriever/ReaderArticleRetrieverFacadeTest.java
@@ -2,18 +2,20 @@ package pl.javaready.readerArtilceRetriever;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ReaderArticleRetrieverFacadeTest {
 
     @Test
-    public void f(){
+    public void shouldReturnArticleWithId1AndContentXXX(){
         //given
         ReaderArticleRetrieverFacade facade = new ReaderArticleRetrieverFacade();
 
         //when
         ArticleDto articleDto = facade.retrieveById(1L);
         //then
+        assert articleDto.id().equals(1L);
+        assert articleDto.content().equals("5 krokow: 1, 2, 3, 4, 5");
+        assert articleDto.header().equals("jak zostac programista");
+        assert articleDto.category().equals("IT");
     }
 
 }
